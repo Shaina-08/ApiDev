5f70486fe8f6c43c8950e8bddef0b47984e494cb
S3b2 <111647190+Shaina-08@users.noreply.github.com>
2024-08-20 09:08:27 +0530

    Started Api Development using python and fastapi

---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"Welcome to my world": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts(payload: dict= Body(...)):
+    print(payload)
+    return {"posts": "This is a post"}
+
+
+# Order does matter , first written function is called first becuase it starts from top of file
+
+
+
