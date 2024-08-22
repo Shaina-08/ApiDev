38d01195a2ba06f99c04ffba7b7ac0f4e3028490
S3b2 <111647190+Shaina-08@users.noreply.github.com>
2024-08-22 19:38:39 +0530

    Test case passed for get_posts

--- test/test_main.py
+++ test/test_main.py
@@ -9,8 +9,17 @@ def test_root():
     assert response.json() == {"Welcome to my world": "Hello World"}
 
 def test_get_posts():
-    payload = {"key": "value"}
-    response = client.post("/posts", json=payload) 
-    assert response.status_code == 200
-    assert response.json() == {"posts": "This is a post"}
-    
+    payload = {
+        "title": "My First Post",
+        "content": "This is the content of my first post",
+    }
+    response = client.post("/posts", json=payload)
+    assert response.status_code == 201
+
+    response_data = response.json()["data"]
+    assert response_data["title"] == payload["title"]
+    assert response_data["content"] == payload["content"]
+    assert "id" in response_data 
+    assert isinstance(response_data["id"], int)
+    assert response_data["published"] is False
+    assert response_data["rating"] is None
